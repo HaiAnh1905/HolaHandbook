2e9d6f0b0f073a16074a0ba0681e1ae2df8fddc6
HaiAnh1905 <haianh198005@gmail.com>
2024-10-11 07:51:47 +0700

    dm SSG

--- CamNangHoLa/src/homepage/index.tsx
+++ CamNangHoLa/src/homepage/index.tsx
@@ -1,12 +1,85 @@
-import Slider from './slider/index.tsx'
-import './style.scss'
+import { useState } from "react";
+import Slider from "./slider/index.tsx";
+import "./style.scss";
 
-export default function HomePage(){
-    return(
-        <div className="homepage">
-            <div className="slider">
-            <Slider/>
-            </div>
+interface sliderArrayItem {
+  index: number,
+  title: String,
+  sentence: String,
+  img: String,
+}
+
+export default function HomePage() {
+    const [sliderImg, setSliderImg] = useState<sliderArrayItem[]>([
+        {   
+            index: 0,
+            title: "Chào mừng các bạn đến với Hòa Lạc",
+            sentence: "Vũ trụ thông tin về Hòa Lạc",
+            img: "https://daihoc.fpt.edu.vn/wp-content/uploads/2021/02/144409980_10164565272570062_7625633052716715320_o.jpg"
+        },
+        {   
+            index: 1,
+            title: "Những sự kiện đầy thú vị",
+            sentence: "blah blah blah blah blah",
+            img: "https://scontent.fhan14-5.fna.fbcdn.net/v/t39.30808-6/462215883_932128148945449_7715940863643974808_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeG1yF3dYji_sl9O4aI2OOWGLxP9FEpSXCsvE_0USlJcKzmIKZd79NYv7yp6QWqgUmD6e87KcrEuwRUU3AoGmP47&_nc_ohc=trazKhT0ipQQ7kNvgG-LMHM&_nc_ht=scontent.fhan14-5.fna&_nc_gid=AMjfuo9iAuJva290gThrq74&oh=00_AYBs9LAc7XwPbv-04GX6f8UkUztBuNKP-C_0KEj3HoY9hg&oe=670BCF71"
+        },
+        {   
+            index: 2,
+            title: "Canteen trường FPT",
+            sentence: "blah blah blah blah blah",
+            img: "https://hoalac-school.fpt.edu.vn/wp-content/uploads/FPTSchool-0081-scaled.jpg"
+        },
+    ]);
+    const [sliderIndex, setSliderIndex] = useState<number>(0);
+    const handleNext = () => {
+      if(sliderIndex === sliderImg.length - 1) {setSliderIndex(0); return}
+      setSliderIndex(sliderIndex + 1);
+    }
+    const handlePrevious = () => {
+      if(sliderIndex === 0) {setSliderIndex(sliderImg.length - 1); return}
+      setSliderIndex(sliderIndex - 1);
+    }
+    
+    // console.log(sliderImg[sliderIndex]);
+  return (
+    <div className="homepage">
+      <div className="relative slider">
+        <div className="arrow flex items-center h-full w-full justify-between absolute z-10">
+          <div onClick={handlePrevious} className="left-arrow h-full flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2.3"
+              stroke="white"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </div>
+          <div onClick={handleNext} className="right-arrow h-full flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2.3"
+              stroke="white"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </div>
         </div>
-    )
+        <Slider sliderUrl = {sliderImg[sliderIndex]}/>
+      </div>
+    </div>
+  );
 }
--- CamNangHoLa/src/homepage/slider/index.tsx
+++ CamNangHoLa/src/homepage/slider/index.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
-export default function Slider() {
-  const [sliderIndex, setSliderIndex] = useState<number>(0);
+interface sliderArrayI {
+  sliderUrl: {
+    index: number;
+    title: String;
+    sentence: String;
+    img: String;
+  };
+}
+
+export default function Slider(sliderUrl: sliderArrayI) {
   return (
     <div
       className="slider-item w-full relative "
@@ -12,16 +20,16 @@ export default function Slider() {
         margin: "0 auto",
       }}
     >
-        <div className="text absolute text-white z-20">
-            <h1 className="text-4xl m-auto font-semibold mb-3">Chào mừng các bạn đến với Hòa Lạc</h1>
-            <div className="intro m-auto text-lg w-fit">blah blah blah blah blah blah</div>
+      <div className="text absolute text-white z-10">
+        <h1 className="text-4xl m-auto font-semibold mb-3 text-center">
+          {sliderUrl.sliderUrl.title}
+        </h1>
+        <div className="intro m-auto text-lg w-fit">
+          {sliderUrl.sliderUrl.sentence}
         </div>
+      </div>
       <div className="op absolute opacity-50 w-full h-full"></div>
-      <img
-        className="slider-img"
-        src="https://daihoc.fpt.edu.vn/wp-content/uploads/2021/02/144409980_10164565272570062_7625633052716715320_o.jpg"
-        alt=""
-      />
+      <img className="slider-img" src={`${sliderUrl.sliderUrl.img}`} alt="" />
     </div>
   );
 }
